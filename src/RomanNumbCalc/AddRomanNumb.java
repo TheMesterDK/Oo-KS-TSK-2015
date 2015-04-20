1383b7faf1d7e0d192dc0aef50c44f320ea7d4d6
TheMesterDK <TheMester.dk@gmail.com>
2015-04-20 22:43:47 +0200

    Second test comit
    Delvis implemteret første test

--- src/RomanNumbCalc/AddRomanNumb.java
+++ src/RomanNumbCalc/AddRomanNumb.java
@@ -1,13 +1,35 @@
 package RomanNumbCalc;
 
 public class AddRomanNumb {
+  // Yoda: "Do or do not, there is no try"
+  public String RomanOne;
+  public String RomanTwo;
+  public String Resultat;
   
-  public String AddRomanNumb(String RomanOne, String RomanTwo){
+  
+  
+  
+  public AddRomanNumb(String One, String Two){
+    //return RomanOne+RomanTwo;
+    this.RomanOne = One;
+    this.RomanTwo = Two;
+  }
+  
+  public void setRomanNumber(String One, String Two){
+    this.RomanOne = One;
+    this.RomanTwo = Two;
+  }
+  
+  public AddRomanNumb() {
+    // TODO Auto-generated constructor stub
+    
+  }
+
+  //get
+  public String adder(){//String  One, String Two){
     return RomanOne+RomanTwo;
   }
   
-  public String Add(String RomanOne, String RomanTwo){
-    return RomanOne+RomanTwo;
-  }
+  
 
 }
--- test/AddRomanNumbersTest.java
+++ test/AddRomanNumbersTest.java
@@ -6,17 +6,40 @@ import org.junit.Test;
 import RomanNumbCalc.AddRomanNumb;
 
 public class AddRomanNumbersTest {
+  
+  
 
   @Before
   public void setUp() throws Exception {
-    new AddRomanNumb();
+    //new AddRomanNumb();
   }
 
   @Test
   public void test() {
-    fail( "Not yet implemented" );
-    assertEquals("IIIIII", Add("III","III") );
-    assertEquals("IIIIII", AddRomanNumb("III","III") );
+    
+    AddRomanNumb addRomanNumb = new AddRomanNumb();
+    
+    addRomanNumb.setRomanNumber( "III", "III" );
+    
+    String TestResultat = addRomanNumb.adder();
+//        adder("III","III") ;
+    
+    //fail( "Not yet implemented" );
+    // 
+    assertEquals("IIIIII", TestResultat);
+    //assertEquals("IIIIII", addRomanNumb.adder("III","III") );
+    //assertEquals("IIIIII", AddRomanNumb("III","III") );
   }
+  
+  @Test
+  public void testEmptyStrings(){
+    
+    AddRomanNumb addRomanNumb = new AddRomanNumb();
+    
+    String TestResultat = addRomanNumb.adder();
+    
+    assertEquals("", TestResultat);
+  }
+  
 
 }
